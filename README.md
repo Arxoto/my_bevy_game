f5019974bcf0b185d44d2b37062b3165c55d1577
Arxoto <1484663586@qq.com>
2024-07-07 23:56:29 +0800

    update a_2d_shapes

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 see [bevy-setup](https://bevyengine.org/learn/quick-start/getting-started/setup/)
 
 see [next-steps](https://bevyengine.org/learn/quick-start/next-steps/)
+
+see [2d-examples](https://github.com/bevyengine/bevy/tree/latest/examples#2d-rendering)
--- src/examples/a_2d_shapes.rs
+++ src/examples/a_2d_shapes.rs
@@ -22,16 +22,29 @@ fn setup(
 ) {
     commands.spawn(Camera2dBundle::default());
 
+    // 将原始图形加入到 Mesh Resource 网格资源中防止被销毁 并返回其句柄
     let shapes = [
+        // 圆
         Mesh2dHandle(meshes.add(Circle { radius: 50.0 })),
+        // 圆扇形 angle 角度即弧度与半径的倍数 如 Pi 等同于整圆
         Mesh2dHandle(meshes.add(CircularSector::new(50.0, 1.0))),
+        // 圆片段 同上
         Mesh2dHandle(meshes.add(CircularSegment::new(50.0, 1.25))),
+        // 椭圆 即圆的拉伸
         Mesh2dHandle(meshes.add(Ellipse::new(25.0, 50.0))),
+        // 圆环
         Mesh2dHandle(meshes.add(Annulus::new(25.0, 50.0))),
+        // 胶囊 与线段在一定距离内的点的集合
+        // 范围半径即两端半圆的半径 线段长度即中间矩形的长度
+        // 实测渲染时中间长度会略长于预期 目前原因尚不清楚
         Mesh2dHandle(meshes.add(Capsule2d::new(25.0, 50.0))),
+        // 菱形 水平对角线 竖直对角线 即宽高
         Mesh2dHandle(meshes.add(Rhombus::new(75.0, 100.0))),
+        // 矩形 宽高
         Mesh2dHandle(meshes.add(Rectangle::new(50.0, 100.0))),
+        // 正多边形 circumradius 表示外接圆半径
         Mesh2dHandle(meshes.add(RegularPolygon::new(50.0, 6))),
+        // 三角形2d
         Mesh2dHandle(meshes.add(Triangle2d::new(
             Vec2::Y * 50.0,
             Vec2::new(-50.0, -50.0),
@@ -44,6 +57,8 @@ fn setup(
         // Distribute colors evenly across the rainbow.
         let color = Color::hsl(360. * i as f32 / num_shapes as f32, 0.95, 0.7);
 
+        // 将材质网格的捆绑包加入到世界中
+        // 位置信息是以窗口正中心为原点的坐标
         commands.spawn(MaterialMesh2dBundle {
             mesh: shape,
             material: materials.add(color),
@@ -57,6 +72,7 @@ fn setup(
         });
     }
 
+    // 添加左上角按键交互的提示信息
     commands.spawn(
         TextBundle::from_section("Press space to toggle wireframes", TextStyle::default())
             .with_style(Style {
